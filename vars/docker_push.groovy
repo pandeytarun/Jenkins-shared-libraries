8a0ca5561df12909db15fcf3f5c8d1f576ea3eab
Tarun Pandey <pandeytarun021@gmail.com>
2025-04-30 02:09:17 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,5 @@ def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId:"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser",)]) {
       sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
   }
-   sh "docker push ${env.dockerHubUser}/node-app:latest"
+   sh "docker push $dockerHubUser/node-app:latest"
  }
